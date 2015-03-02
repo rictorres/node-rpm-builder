6376ffae1a0fe24aa7ae49654a7e12978e45080d
Ricardo Torres <ricardo@rictorres.com.br>
2015-03-02 09:18:18 +0000

    Improve readability/maintainability
    Decouple functions to make the code more human friendly.

--- index.js
+++ index.js
@@ -10,7 +10,99 @@ var _ = require('lodash');
 
 var writeSpec = require('./lib/spec');
 
-function build(options, cb) {
+function setupTempDir(tmpDir) {
+  var rpmStructure = ['BUILD', 'BUILDROOT', 'RPMS', 'SOURCES', 'SPECS', 'SRPMS'];
+
+  // If the tmpDir exists (probably from previous build), delete it first
+  if (fsx.existsSync(tmpDir)) {
+    console.log(chalk.cyan('Removing old temporary directory.'));
+    fsx.removeSync(tmpDir);
+  }
+
+  // Create RPM folder structure
+  console.log(chalk.cyan('Creating RPM directory structure at:'), tmpDir);
+  _.forEach(rpmStructure, function(dirName) {
+    fsx.mkdirpSync(path.join(tmpDir, dirName));
+  });
+}
+
+function retrieveFilesToExclude(excludeFiles) {
+  return globby.sync(excludeFiles).map(function(file) {
+    return path.normalize(file);
+  });
+}
+
+function prepareFiles(files, excludeFiles, buildRoot) {
+  var _files = [];
+  var filesToExclude = retrieveFilesToExclude(excludeFiles);
+
+  _.forEach(files, function(file) {
+    if (!file.hasOwnProperty('src') || !file.hasOwnProperty('dest')) {
+      var err = new Error('All files/folders must have source (src) and destination (dest) set');
+      return cb(err);
+    }
+
+    file.cwd = (file.cwd || '.') + '/';
+
+    var actualSrc = globby.sync(path.join(file.cwd, file.src));
+
+    fsx.ensureDir(path.join(buildRoot, file.dest));
+
+    _.forEach(actualSrc, function(srcFile) {
+      // Check whether to ignore this file
+      if (filesToExclude.indexOf(srcFile) > -1) {
+        return;
+      }
+
+      // files/folders should be copied
+      // taking into account the cwd
+      // so the destination should be
+      // relative to the cwd
+      var copyTarget = path.normalize(srcFile).replace(path.normalize(file.cwd), '');
+      var dest = path.join(file.dest, copyTarget);
+      _files.push(dest);
+      fsx.copySync(srcFile, path.join(buildRoot, dest));
+    });
+  });
+
+  return _files;
+}
+
+function buildRpm(buildRoot, specFile, rpmDest, cb) {
+  // Build the RPM package.
+  var cmd = [
+    'rpmbuild',
+    '-bb',
+    '-vv',
+    '--buildroot',
+    buildRoot,
+    specFile
+  ].join(' ');
+
+  console.log(chalk.cyan('Executing:'), cmd);
+
+  exec(cmd, {}, function rpmbuild(err, stdout) {
+
+    if (err) {
+      return cb(err);
+    }
+
+    if (stdout) {
+      var rpm = stdout.match(/(\/.+\..+\.rpm)/);
+      if (rpm && rpm.length > 0) {
+        if (rpmDest) {
+          var rpmDestination = path.join(rpmDest, path.basename(rpm[0]));
+          console.log(chalk.cyan('Copying RPM package to:'), rpmDestination);
+          fsx.copySync(rpm[0], rpmDestination);
+        }
+
+        return cb(null, rpmDestination);
+      }
+    }
+  });
+}
+
+module.exports = function(options, cb) {
   if (!options || typeof options !== 'object') {
     throw new TypeError('options object is missing');
   }
@@ -40,97 +132,26 @@ function build(options, cb) {
 
   var tmpDir = path.resolve(options.tempDir);
   var buildRoot = path.join(tmpDir, '/BUILDROOT/');
-  var rpmStructure = ['BUILD', 'BUILDROOT', 'RPMS', 'SOURCES', 'SPECS', 'SRPMS'];
 
-  // If the tmpDir exists (probably from previous build), delete it first
-  if (fsx.existsSync(tmpDir)) {
-    console.log(chalk.cyan('Removing old temporary directory.'));
-    fsx.removeSync(tmpDir);
-  }
+  setupTempDir(tmpDir);
 
-  // Create RPM folder structure
-  console.log(chalk.cyan('Creating RPM directory structure at:'), tmpDir);
-  _.forEach(rpmStructure, function(dirName) {
-    fsx.mkdirpSync(path.join(tmpDir, dirName));
-  });
-
-  var excludeFiles = globby.sync(options.excludeFiles).map(function(file) {
-    return path.normalize(file);
-  });
-
-  // Copy source files to the BUILDROOT folder
-  var files = [];
-  _.forEach(options.files, function(file) {
-    if (!file.hasOwnProperty('src') || !file.hasOwnProperty('dest')) {
-      var err = new Error('All files/folders must have source (src) and destination (dest) set');
-      return cb(err);
-    }
-
-    file.cwd = (file.cwd || '.') + '/';
-
-    var actualSrc = globby.sync(path.join(file.cwd, file.src));
-
-    fsx.ensureDir(path.join(buildRoot, file.dest));
-
-    _.forEach(actualSrc, function(srcFile) {
-      // Check whether to ignore this file
-      if (excludeFiles.indexOf(srcFile) > -1) {
-        return;
-      }
-
-      // files/folders should be copied
-      // taking into account the cwd
-      // so the destination should be
-      // relative to the cwd
-      var copyTarget = path.normalize(srcFile).replace(path.normalize(file.cwd), '');
-      var dest = path.join(file.dest, copyTarget);
-      files.push(dest);
-      fsx.copySync(srcFile, path.join(buildRoot, dest));
-    });
-  });
+  var files = prepareFiles(options.files, options.excludeFiles, buildRoot);
 
   // Write spec file
   var specFile = writeSpec(files, options);
   console.log(chalk.cyan('SPEC file created:'), specFile);
 
-  // Build the RPM package.
-  var cmd = [
-    'rpmbuild',
-    '-bb',
-    '-vv',
-    '--buildroot',
-    buildRoot,
-    specFile
-  ].join(' ');
-
-  console.log(chalk.cyan('Executing:'), cmd);
-
-  exec(cmd, {}, function rpmbuild(err, stdout) {
-
+  buildRpm(buildRoot, specFile, options.rpmDest, function(err, rpm) {
     if (err) {
       return cb(err);
     }
 
-    if (stdout) {
-      var rpm = stdout.match(/(\/.+\..+\.rpm)/);
-      if (rpm && rpm.length > 0) {
-        if (options.rpmDest) {
-          var rpmDestination = path.join(options.rpmDest, path.basename(rpm[0]));
-          console.log(chalk.cyan('Copying RPM package to:'), rpmDestination);
-          fsx.copySync(rpm[0], rpmDestination);
-        }
-
-        // Remove temp folder
-        if (!options.keepTemp) {
-          console.log(chalk.cyan('Removing RPM directory structure at:'), tmpDir);
-          fsx.removeSync(tmpDir);
-        }
-
-        return cb(null, rpmDestination);
-      }
+    // Remove temp folder
+    if (!options.keepTemp) {
+      console.log(chalk.cyan('Removing RPM directory structure at:'), tmpDir);
+      fsx.removeSync(tmpDir);
     }
 
+    return cb(null, rpm);
   });
 }
-
-module.exports = build;
